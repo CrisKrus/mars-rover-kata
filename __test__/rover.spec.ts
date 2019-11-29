22bce37f99ae71b72f9cc1645fe9bcb76cde072a
Cristian Suárez Vera <cristian.suarez@leanmind.es>
2019-11-29 10:00:59 +0000

    Green: Rover rotate to west

--- __test__/rover.spec.ts
+++ __test__/rover.spec.ts
@@ -10,7 +10,11 @@ class Point {
 
 class Direction {
   facingNorth() {
-    return '';
+    return 'N';
+  }
+
+  facingWest() {
+    return 'W';
   }
 }
 
@@ -21,6 +25,10 @@ class Position {
   constructor(point: Point, direction: string) {
     this.point = point;
     this.direction = direction;
+  }
+
+  getPoint() {
+    return this.point;
   }
 }
 
@@ -40,17 +48,37 @@ class Rover {
   currentPosition(): Position {
     return this.position;
   }
+
+  execute(command: string) {
+    const currentPoint = this.position.getPoint();
+    const newDirection = new Direction().facingWest();
+
+    return new Rover(new Position(currentPoint, newDirection));
+  }
 }
 
 describe('Rover', () => {
   it('should start on an initial point and facing a direction', () => {
     const rover = Rover.startFacingNorth();
+    rover.execute('L');
 
     const expectedPosition = new Position(
       new Point(0, 0),
       new Direction().facingNorth(),
     );
 
+    expect(rover.currentPosition()).toStrictEqual(expectedPosition);
+  });
+
+  it('should rotate itself', () => {
+    const rover = Rover.startFacingNorth();
+    // commands: L => rover facing W
+    rover.execute('L');
+
+    const expectedPosition = new Position(
+      new Point(0, 0),
+      new Direction().facingWest(),
+    );
     expect(rover.currentPosition()).toStrictEqual(expectedPosition);
   });
 });
